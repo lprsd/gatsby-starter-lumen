912704ea0e8c4816aee637803388f020deed72a7
EC2 Default User <ec2-user@ip-172-31-25-249.us-east-2.compute.internal>
2019-02-09 18:10:03 +0000

    Updated titles

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  url: 'https://rnstnqry.com',
+  title: 'Earnest Inquiry',
+  subtitle: 'in the pursuit of Freedom and other entrained endeavours',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-134170545-1',
   menu: [
     {
       label: 'Articles',
